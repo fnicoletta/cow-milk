d90d7bbcb7ea07636a0b98247d8f6938b79c76af
TNORTHERN <traynorthern@yahoo.com>
2020-02-05 20:49:04 -0600

    start on navigation responsiveness

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,6 +2,14 @@
   <div>
     <Navbar />
     <slot />
+    <div class="navbar__bottom">
+      <div class="navbar__bottom__container">
+        <div class="navbar__bottom__link">Link1</div>
+        <div class="navbar__bottom__link">Link1</div>
+        <div class="navbar__bottom__link">Link1</div>
+        <div class="navbar__bottom__link">Link1</div>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
