0ef4522b57a18cc8819c3be03e08402c2c69e9f4
TNORTHERN <traynorthern@yahoo.com>
2020-02-06 11:23:34 -0600

    some navbar updates

---
+++ src/components/Misc/Panel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="panel">
+    <span @click="close" class="panel__close">X</span>
+
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    close: {
+      type: Function,
+      default: () => {}
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/components/NavbarPanel.vue
@@ -0,0 +1,70 @@
+<template>
+  <Panel :close="close">
+    <div class="navbar-panel">
+      <h3>
+        <template v-if="$store.state.auth.user">
+          {{ $store.state.auth.user.name }}</template
+        >
+      </h3>
+      <ul>
+        <router-link to="/orders">
+          <li>
+            Orders
+          </li>
+        </router-link>
+        <router-link to="/settings">
+          <li>
+            Settings
+          </li>
+        </router-link>
+        <li @click="logout">
+          Logout
+        </li>
+      </ul>
+    </div>
+  </Panel>
+</template>
+
+<script>
+import Panel from "@/components/Misc/Panel";
+export default {
+  components: {
+    Panel
+  },
+  props: {
+    close: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  methods: {
+    logout() {
+      this.loading = true;
+      const currentToken = this.getCookie("jwt-token");
+      this.setCookie("jwt-token", "", 0);
+      const dataToPass = {
+        api_token: 12345
+      };
+      const headers = {
+        headers: {
+          Authorization: `Bearer ${currentToken}`
+        }
+      };
+      this.$edgewood
+        .post("signout", dataToPass, headers)
+        .then(({ data }) => {
+          this.loading = false;
+          this.$store.commit("auth/setUser", null);
+          this.$router.push("/login");
+        })
+        .catch(err => {
+          this.loading = false;
+          console.log(err);
+          alert("something went wrong...");
+        });
+    }
+  }
+};
+</script>
+
+<style></style>
